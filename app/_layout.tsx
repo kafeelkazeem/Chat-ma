52b47602672d32dd8dbe52238603bf440d3ce300
kafeelkazeem <kafeelkazeem@gmail.com>
2025-08-13 21:02:41 +0100

    created chatRoom file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -56,6 +56,7 @@ function RootLayoutNav() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="chatRoom" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
     </>
---
+++ app/chatRoom.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function ChatRoom(){
+    return(
+        <View>
+            <Text>ChatRoom</Text>
+        </View>
+    ) 
+}
